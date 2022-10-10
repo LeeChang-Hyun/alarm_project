39ad0771634f58fe718b4aedefaff3d9a9857e47
ChangHyun <sesfinkl@hanmir.com>
2022-10-10 23:09:22 +0900

    feat: add package flutter_local_notifications

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('btn'),
+            ),
+            const Center(child: Text('hi')),
+          ],
+        ),
+      ),
+    );
+  }
+}
